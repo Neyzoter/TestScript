c684abdaa1a51066ff1a9669d993153fe5e2eace
Neyzoter <sonechaochao@gmail.com>
2019-11-04 10:57:11 +0800

    add mongo export

---
+++ sql/mongodb/README.md
@@ -0,0 +1,5 @@
+# mongodb
+
+* mongoexport.sh
+
+export data as json file from mongodb.
---
+++ sql/mongodb/mongoexport.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# mongoexport -h 主机地址 -d 数据库名称 -c 集合名称 --csv --field 字段列表 -o 输出地址
+mongoexport -h 127.0.0.1 -d udp -c 2019-07 -o ./mongoexportdata/2019-07.json
